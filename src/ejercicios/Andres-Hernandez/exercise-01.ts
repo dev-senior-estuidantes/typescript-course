d23f4ae494884885ea716e3c70b0cf82dfec8169
Andres Hernandez <andres.hernandez74@est.iudigital.edu.co>
2025-05-14 00:38:54 -0500

    CHANGES: exercise-01, prompt

--- src/ejercicios/Andres-Hernandez/exercise-01.ts
+++ src/ejercicios/Andres-Hernandez/exercise-01.ts
@@ -2,15 +2,19 @@
 •	Objetivo: Comprender la declaración de variables y operadores aritméticos básicos.
 •	Pregunta: ¿Cómo podrías modificar este código para que la suma sea ingresada por el usuario? */
 
+
 let num1: number = 10;
 let num2: number = 10;
 let suma: number = num1 + num2;
 console.log(suma);
 
 // Modificación para que los numeros sean ingresados por el usuario
-let number1: number = Number(prompt("Ingresa el primer número:"));
-let number2: number = Number(prompt("Ingresa el segundo número:"));
+const promptSync = require('prompt-sync')();
+
+let number1: number = Number(promptSync("Ingresa el primer número:"));
+let number2: number = Number(promptSync("Ingresa el segundo número:"));
 let sum: number = number1 + number2;
 console.log(`La suma de ${number1} y ${number2} es: ${sum}`);
+
 
 // Se utiliza Number() para convertir la entrada del usuario (que es un string) a un número
