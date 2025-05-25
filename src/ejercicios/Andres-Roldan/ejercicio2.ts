8a6e160ba224611ba15b0af06e9adbf1f56ccb60
andrewroldanx <mandvalencia@poligran.edu.co>
2025-05-24 19:55:07 -0500

    agregar texto

--- src/ejercicios/Andres-Roldan/ejercicio2.ts
+++ src/ejercicios/Andres-Roldan/ejercicio2.ts
@@ -4,6 +4,8 @@
 
 let palabra1:String = "hola"
 let palabra2: number = 190
+let palabra3: String= palabra1+ "el numero es" + palabra2
 
-console.log(palabra1, palabra2)
+console.log(palabra3)
 
+//Hay error al querer unir un string con un number, en este caso si lo asignamos a otra variable para concatenar el number queda como string
